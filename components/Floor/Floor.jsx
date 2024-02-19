433091f5db4976ae5cf9dadd7031d30329c14866
Charis Amend <charis.amend@googlemail.com>
2024-02-19 16:32:25 +0100

    add three to transpilePackages property in next.config.js

--- components/Floor/Floor.jsx
+++ components/Floor/Floor.jsx
@@ -1,8 +1,9 @@
-import { extend } from '@react-three/fiber'
-import { BoxBufferGeometry } from 'three-stdlib'
-extend({ BoxBufferGeometry })
-
-export default function Floor({ props }) {
+import { extend } from "react-three-fiber"
+export default function Floor({ ...props }) {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh)
     return (
         <mesh {...props} receiveShadow>
             <boxBufferGeometry args={[20, 1, 10]} />
