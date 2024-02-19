1e75780ed2e436a82f6814cc66cf78d52ea4017a
Charis Amend <charis.amend@googlemail.com>
2024-02-19 16:40:17 +0100

    added reactdom render function and a jar function into the glassJar component

--- components/GlassJar/GlassJar.jsx
+++ components/GlassJar/GlassJar.jsx
@@ -1,20 +1,43 @@
 import Floor from "../Floor/Floor"
 import { Canvas } from "@react-three/fiber"
+import ReactDOM from 'https://esm.sh/react-dom'
+import React, { useRef, useState } from 'https://esm.sh/react'
+import { Canvas, useFrame } from 'https://esm.sh/@react-three/fiber'
+import htm from 'https://esm.sh/htm'
+
+const html = htm.bind(React.createElement)
 
 export default function GlassJar() {
 
-    return (
-        <>
-            <div className="scene3D w-screen h-auto">
-                <Canvas
-                    shadows
-                    className="canvas3D bg-transparent"
-                    camera={{ position: [-6, 7, 7] }}>
-                    <Floor />
-                </Canvas>
-            </div>
 
-            <p>Glass Jar 3D Object</p>
-        </>
+    function Jar(props) {
+        const meshRef = useRef()
+        const [hovered, setHover] = useState(false)
+        const [active, setActive] = useState(false)
+        useFrame(() => (meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01))
+        return html` <mesh
+        ...${props}
+        ref=${meshRef}
+        scale=${active ? 1.5 : 1}
+        onClick=${() => setActive(!active)}
+        onPointerOver=${() => setHover(true)}
+        onPointerOut=${() => setHover(false)}
+      >
+        <boxGeometry args=${[1, 1, 1]} />
+        <meshStandardMaterial color=${hovered ? 'hotpink' : 'orange'} />
+      </mesh>`
+    }
+    ReactDOM.render(
+
+        html` <${Canvas}>
+                <ambientLight />
+                <pointLight position=${[10, 10, 10]} />
+                <${Jar} position=${[-1.2, 0, 0]} />
+                <${Jar} position=${[1.2, 0, 0]} />
+            <//>`,
+        document.getElementById('root'),
     )
 }
+
+
+
