148c36d9c9e83b527c35af3296f3cd3a2be9c4c9
Charis Amend <charis.amend@googlemail.com>
2024-02-20 18:38:02 +0100

    addded logo

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -22,7 +22,7 @@ export default NextAuth({
                 await transport.sendMail({
                     to: email,
                     from,
-                    subject: `Sign in to ${host}`,
+                    subject: `Your Sign in | Gratitude Jar`,
                     text: text({ url, host }),
                     html: html({ url, host, email }),
                 })
@@ -56,194 +56,67 @@ function html({ url, host, email }) {
     const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`
     const escapedHost = `${host.replace(/\./g, "&#8203;.")}`
 
+    // Some simple styling options
+    const backgroundColor = "#232323"
+    const textColor = "#444444"
+    const mainBackgroundColor = "#ffffff"
+    const buttonBackgroundColor = "#473AEB"
+    const buttonBorderColor = "#346df1"
+    const buttonTextColor = "#ffffff"
+
     return `
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-                <style type="text/css">
-                    #outlook a {
-                        padding: 0;
-    }
-
-                    body {
-                        margin: 0;
-                    padding: 0;
-                    -webkit-text-size-adjust: 100%;
-                    -ms-text-size-adjust: 100%;
-    }
-
-                    table,
-                    td {
-                        border - collapse: collapse;
-                    mso-table-lspace: 0pt;
-                    mso-table-rspace: 0pt;
-    }
-
-                    img {
-                        border: 0;
-                    height: auto;
-                    line-height: 100%;
-                    outline: none;
-                    text-decoration: none;
-                    -ms-interpolation-mode: bicubic;
-    }
-
-                    p {
-                        display: block;
-                    margin: 13px 0;
-    }
-                </style>
-                <!--[if mso]>
-                <noscript>
-                    <xml>
-                        <o:OfficeDocumentSettings>
-                            <o:AllowPNG />
-                            <o:PixelsPerInch>96</o:PixelsPerInch>
-                        </o:OfficeDocumentSettings>
-                    </xml>
-                </noscript>
-                <![endif]-->
-                <!--[if lte mso 11]>
-                <style type="text/css">
-                    .mj-outlook-group-fix {width:100% !important; }
-                </style>
-                <![endif]-->
-                <style type="text/css">
-                    @media only screen and (min-width:480px) {
-      .mj - column - per - 100 {
-                        width: 100% !important;
-                    max-width: 100%;
-      }
-    }
-                </style>
-                <style media="screen and (min-width:480px)">
-                    .moz-text-html .mj-column-per-100 {
-                        width: 100% !important;
-                    max-width: 100%;
-    }
-                </style>
-                <style type="text/css">
-                    @media only screen and (max-width:480px) {
-                        table.mj - full - width - mobile {
-                        width: 100% !important;
-      }
-
-                    td.mj-full-width-mobile {
-                        width: auto !important;
-      }
-    }
-                </style>
-            </head>
-            <body style="word-spacing:normal;background-color:#F4F4F4;">
-                <div style="background-color:#F4F4F4;">
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#000000" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-                        <div style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
-                            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;">
-                                <tbody>
-                                    <tr>
-                                        <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
-                                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-                                                <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                                                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
-                                                        <tbody>
-                                                            <tr>
-                                                                <td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:30px;word-break:break-word;">
-                                                                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
-                                                                        <tbody>
-                                                                            <tr>
-                                                                                <td style="width:100px;">
-                                                                                    <img height="auto" src="/finaljarlogo.png" style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" title="" width="50" />
-                                                                                </td>
-                                                                            </tr>
-                                                                        </tbody>
-                                                                    </table>
-                                                                </td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#000000" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-                            <div style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
-                                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;">
-                                    <tbody>
-                                        <tr>
-                                            <td style="direction:ltr;font-size:0px;padding:0 0 0 0;text-align:center;">
-                                                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-                                                    <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                                                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td align="left" style="font-size:0px;padding:10px 25px;padding-top:25px;padding-bottom:5px;word-break:break-word;">
-                                                                        <div style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;">
-                                                                            <p style="line-height: 40px; text-align: center; margin: 10px 0;font-size:55px;color:#fcfcfc;font-family:'Arial,sans-serif"><b>${escapedHost}</b></p>
-                                                                        </div>
-                                                                    </td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td align="left" style="font-size:0px;padding:10px 25px;padding-top:0px;padding-bottom:20px;word-break:break-word;">
-                                                                        <div style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;">
-                                                                            <p style="line-height: 30px; text-align: center; margin: 10px 0;color:#f5f5f5;font-size:25px;font-family:'Times New Roman',Helvetica,Arial,sans-serif">
-                                                                                <span style="color:#ffffff;font-size:18px;font-family:'Times New Roman',Helvetica,Arial,sans-serif"> Sign in to Gratitude Jar </span>
-                                                                            </p>
-                                                                        </div>
-                                                                    </td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                    <!--[if mso | IE]></td></tr></table><![endif]-->
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#000000" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-                                <div style="background:#000000;background-color:#000000;margin:0px auto;max-width:600px;">
-                                    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#000000;background-color:#000000;width:100%;">
-                                        <tbody>
-                                            <tr>
-                                                <td style="direction:ltr;font-size:0px;padding:0 0 0 0;padding-bottom:40px;text-align:center;">
-                                                    <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-                                                        <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                                                            <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td align="center" style="padding: 20px 0;">
-                                                                            <table border="0" cellspacing="0" cellpadding="0">
-                                                                                <tr>
-                                                                                    <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                                                                                        target="_blank"
-                                                                                        style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                                                                                        in</a></td>
-                                                                                </tr>
-                                                                            </table>
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td align="left" style="font-size:0px;padding:10px 25px;padding-top:5px;padding-bottom:0px;word-break:break-word;">
-                                                                            <div style="font-family:Arial, sans-serif;font-size:13px;line-height:22px;text-align:left;color:#55575d;">
-                                                                                <p style="line-height: 16px; text-align: center; margin: 10px 0;font-size:12px;color:#ffffff;font-family:'Times New Roman',Helvetica,Arial,sans-serif">If you did not request this email you can safely ignore it.</p>
-                                                                            </div>
-                                                                        </td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
-                                                        </div>
-                                                        <!--[if mso | IE]></td></tr></table><![endif]-->
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <!--[if mso | IE]></td></tr></table><![endif]-->
-                </div>
-            </body>
-            `
+  <body style="background: ${backgroundColor};">
+  <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                  <tbody>
+                    <tr>
+                      <td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:30px;word-break:break-word;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                          <tbody>
+                            <tr>
+                              <td style="width:180px;">
+                                <img height="auto" src="https://raw.githubusercontent.com/charis-amend/Gratitude-Jar/main/finaljarlogo.png" style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" title="" width="180" />
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+          <strong>GRATITUDE JAR</strong>
+        </td>
+      </tr>
+    </table>
+    <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+      <tr>
+        <td align="center" style="padding: 10px 0px 0px 0px; font-size: 13px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+          Sign in with your e-mail: ${escapedEmail}
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="padding: 20px 0;">
+          <table border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}">
+              <a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">
+              Sign in 
+              </a>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+          If you did not request this email you can safely ignore it.
+        </td>
+      </tr>
+    </table>
+  </body>
+  `
 }
 
 // Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
