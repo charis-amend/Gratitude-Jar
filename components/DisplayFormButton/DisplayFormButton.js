0a16a1fe05b8311a18eaabe80de7df1b130a1063
Charis Amend <charis.amend@googlemail.com>
2024-02-20 19:21:11 +0100

    displayform button and random gratitude button

---
+++ components/DisplayFormButton/DisplayFormButton.js
@@ -0,0 +1,17 @@
+export default function DisplayFormButton() {
+    return (
+        <>
+            <button
+                class="displayformbutton bg-blue-500 border border-gray-900   text-white font-bold py-3 px-6 rounded-full shadow-lg"
+                type="button"
+                id="displayFormButton"
+                name="displayFormButton">
+                Add Gratitude
+            </button>
+        </>
+    )
+}
+
+
+
+
---
+++ components/RandomGratitudeButton/RandomGratitudeButton.js
@@ -0,0 +1,14 @@
+export default function RandomGratitudeButton() {
+    return (
+        <>
+            <button class="randombutton bg-white text-black px-4 py-2 rounded-md border border-gray-300 hover:bg-gray-100 focus:outline-none focus:border-blue-300"
+                type="button"
+                id="RandomGratitudeButton">
+                Get Random Gratitude Memory
+            </button>
+        </>
+    )
+}
+
+
+
