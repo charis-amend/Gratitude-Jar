fe7bf8b634a1a192a48870e7b6e838e79989b357
Charis Amend <charis.amend@googlemail.com>
2024-02-21 19:39:51 +0100

    some css

--- components/GratitudeForm/GratitudeForm.js
+++ components/GratitudeForm/GratitudeForm.js
@@ -22,22 +22,23 @@ export default function GratitudeForm({ onGratitudeSubmit }) {
             <form
             // onSubmit={(e) => onGratitudeSubmit(e)}
             >
-                <label htmlFor="formTextInput">
-                </label>
-                <textarea
-                    name="formStatementText"
-                    id="formTextInput"
-                    placeholder="What are you grateful for today...?"
-                    autoFocus={true}
-                    cols={10}
-                    rows={7}
-                    maxLength={150}
-                    required
-                ></textarea>
-                <button type="submit">
+                <div className="flex items-center border-b border-teal-500 py-2">
+                    <label htmlFor="formTextInput">
+                    </label>
+                    <input
+                        name="formStatementText"
+                        id="formTextInput"
+                        placeholder="What are you grateful for today...?"
+                        maxLength={150}
+                        required
+                        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    ></input>
+                </div>
+                <button type="submit"
+                    className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" >
                     ADD
                 </button>
-            </form>
+            </form >
         </>
     )
 }
