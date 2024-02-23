2b3c40acb4c6654dcefcb71582b24b65b11fe836
Charis Amend <charis.amend@googlemail.com>
2024-02-23 14:36:04 +0100

    added unauthenticated logic in login component, deleted commented out auth component in app.js and reduced index.js Homepage, and extended users page and added all components

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -5,9 +5,18 @@ import { useEffect } from "react"
 
 
 export default function Login() {
-    // if data: session then...
     const { data: session, status } = useSession()
     const router = useRouter()
+    console.log("----- session in login component:", session, "----- router in login component:", router)
+
+    // if data: session then... 
+    // ↪ loading -> Loading Page return()
+    // ↪ authenticated -> route push to users-page.js and return() signedin as + signout button
+    // ↪ unauthenticated -> route push to index.js and return() not signed in + signin button
+
+    // if data: null or undefined then: 
+
+
     if (session) {
         if (status === "loading") {
             return (
@@ -23,8 +32,8 @@ export default function Login() {
                     </div >
                 </>)
         }
-        useEffect(() => {
 
+        useEffect(() => {
             if (status === "authenticated") {
                 router.replace("/users-page") // lead user to UsersPage users-page.js
                 return (
@@ -41,34 +50,20 @@ export default function Login() {
             }
         }, [session, router])
 
-
-
-
+        if (status === "unauthenticated") {
+            router.replace("/")
+            return (
+                <>
+                    <p
+                        className="signedin-text text-white text-xs w-fit text-right my-3">
+                        Not signed in
+                    </p>
+                    <button type="button"
+                        onClick={() => signIn()}
+                        className="signout-button place-self-end text-white text-xs bg-blue-950/70 w-fit border-1 rounded-xl py-2 px-3 my-1 shadow-md">Sign In
+                    </button>
+                </>
+            )
+        }
     }
-
-}
-
-
-
-
-
-
-console.log("----- session in login component:", session)
-const userAuthenticated = useSession(status === "authenticated")
-console.log("----- user is authenticated in login component:", userAuthenticated)
-
-if (session) {
-}
-return (
-    <>
-        <p
-            className="signedin-text text-white text-xs w-fit text-right my-3">
-            Not signed in
-        </p>
-        <button type="button"
-            onClick={() => signIn()}
-            className="signout-button place-self-end text-white text-xs bg-blue-950/70 w-fit border-1 rounded-xl py-2 px-3 my-1 shadow-md">Sign In
-        </button>
-    </>
-)
 }
--- src/pages/users-page.js
+++ src/pages/users-page.js
@@ -1,47 +1,35 @@
 import { useSession } from "next-auth/react"
+// Components:
+import Login from "../../components/Login/Login";
+import GlassJar from "../../components/GlassJar/GlassJar";
+import GratitudeForm from "../../components/GratitudeForm/GratitudeForm";
+import GratitudeStatement from "../../components/GratitudeStatement/GratitudeStatement";
+import DisplayFormButton from "../../components/DisplayFormButton/DisplayFormButton";
+import RandomGratitudeButton from "../../components/RandomGratitudeButton/RandomGratitudeButton";
+// -------------------------
 
 export default function UsersPage() {
-    try {
-        // if data: session then...
-        const { data: session, status } = useSession()
+    return (
+        <>
+            <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
+                <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" />
+                {/* image for the loading page showing this image before the 3D object has loaded 
+          <Image src="/imgGlasJar.png" alt="placeholder glasjar" width={400} height={600} /> */}
+                <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
+                    <Login />
+                </div>
+                {/* viewbox for active displayed gratitude statement and blurry background: */}
+                <GratitudeStatement />
+                {/* lower section: */}
+                <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
+                    <DisplayFormButton />
+                    <GratitudeForm />
+                    <RandomGratitudeButton />
+                </div >
+            </div >
 
-        if (status === "loading") {
-            // return "Loading ... 🫙"
-            return (
-                <>
-                    <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex flex-col justify-center items-center">
-                        <h1 className="loading-heading text-center p-2 text-blue-50 align-middle mt-10">
-                            Loading your jar ... 🫙
-                        </h1>
-                        <div className="img-div w-full, h-full relative">
-                            <Image src="/imgGlasJar.png" alt="placeholder glasjar" layout='fill'
-                                objectFit='contain' />
-                        </div>
-                    </div >
-                </>)
-        }
-        if (status === "authenticated") {
-            return (
-
-
-
-
-
-
-
-                
-            )
-        }
-
-
-
-    } catch (error) {
-
-    }
-
-
-
-    return
+        </>
+    )
 }
 
 // signOut()
