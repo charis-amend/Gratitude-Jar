03177da1d935371649b2a8000a81dd1734d91f1e
Charis Amend <charis.amend@googlemail.com>
2024-02-23 23:24:48 +0100

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,29 +9,62 @@ import GratitudeForm from "../../components/GratitudeForm/GratitudeForm";
 import GratitudeStatement from "../../components/GratitudeStatement/GratitudeStatement";
 import DisplayFormButton from "../../components/DisplayFormButton/DisplayFormButton";
 import RandomGratitudeButton from "../../components/RandomGratitudeButton/RandomGratitudeButton";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import SignInButton from "../../components/SignInButton/SignInButton";
 // -------------------------
 
 export default function Home() {
-  return (
-    <>
-      <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
-        <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" />
-        {/* image for the loading page showing this image before the 3D object has loaded 
+  const { data: session, status } = useSession()
+  const [isLoggedInUser, setIsLoggedInUser] = useState(status === 'authenticated');
+  console.log("-----isLoggedInUser-----", isLoggedInUser)
+
+  if (isLoggedInUser) {
+    return (
+      <>
+        <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
+          <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" />
+          {/* image for the loading page showing this image before the 3D object has loaded 
           <Image src="/imgGlasJar.png" alt="placeholder glasjar" width={400} height={600} /> */}
-        <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
-          <Login />
-        </div>
-        {/* viewbox for active displayed gratitude statement and blurry background: */}
-        <GratitudeStatement />
-        {/* lower section: */}
-        <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
-          <DisplayFormButton />
-          <GratitudeForm />
-          <RandomGratitudeButton />
+          <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
+            <Login />
+          </div>
+          {/* viewbox for active displayed gratitude statement and blurry background: */}
+          <GratitudeStatement />
+          {/* lower section: */}
+          <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
+            <DisplayFormButton />
+            <GratitudeForm />
+            <RandomGratitudeButton />
+          </div >
         </div >
-      </div >
-    </>
-  )
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
+          <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" />
+          {/* image for the loading page showing this image before the 3D object has loaded 
+          <Image src="/imgGlasJar.png" alt="placeholder glasjar" width={400} height={600} /> */}
+          <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
+            <Login />
+          </div>
+          {/* lower section: */}
+          <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
+            <p
+              className="statement text-center text-xs p-2 text-blue-50 place-self-start h-full"
+            >
+              Login here, to add your gratitude statements, so no one else can see them. We will only send you a link to your e-mail to log you in. None of your data will be processed.
+            </p>
+            <SignInButton />
+            <Login />
+          </div >
+        </div >
+      </>
+    )
+
+  }
 }
 
-Home.auth = true;
+// Home.auth = true;
