4357451f14f44fb59c0de6e148c80fd3a3ba8d5d
Charis Amend <charis.amend@googlemail.com>
2024-02-23 16:13:48 +0100

    changed userspage to have required true for usession and usession for index homepage

--- src/pages/users-page.js
+++ src/pages/users-page.js
@@ -23,29 +23,30 @@ export default function UsersPage() {
                     </div>
                 </div >
             </>)
-    }
+    } else {
 
-    return (
-        <>
-            <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
-                {/* <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" /> */}
-                {/* image for the loading page showing this image before the 3D object has loaded 
+        return (
+            <>
+                <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
+                    <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" />
+                    {/* image for the loading page showing this image before the 3D object has loaded 
           <Image src="/imgGlasJar.png" alt="placeholder glasjar" width={400} height={600} /> */}
-                <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
-                    <Login />
-                </div>
-                {/* viewbox for active displayed gratitude statement and blurry background: */}
-                <GratitudeStatement />
-                {/* lower section: */}
-                <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
-                    <DisplayFormButton />
-                    <GratitudeForm />
-                    <RandomGratitudeButton />
+                    <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
+                        <Login />
+                    </div>
+                    {/* viewbox for active displayed gratitude statement and blurry background: */}
+                    <GratitudeStatement />
+                    {/* lower section: */}
+                    <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
+                        <DisplayFormButton />
+                        <GratitudeForm />
+                        <RandomGratitudeButton />
+                    </div >
                 </div >
-            </div >
 
-        </>
-    )
+            </>
+        )
+    }
 }
 
 
