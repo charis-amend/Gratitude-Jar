7a895710508cfdbe595aedff379cbf65fb2c130f
Charis Amend <charis.amend@googlemail.com>
2024-02-24 19:46:07 +0100

    added submission functions in form component

--- components/GratitudeForm/GratitudeForm.js
+++ components/GratitudeForm/GratitudeForm.js
@@ -1,58 +1,52 @@
 import { useSession } from "next-auth/react"
 
 export default function GratitudeForm({ onGratitudeSubmit }) {
-    const { data: session, status } = useSession()
-    // data can be Session / undefined / null
+    const { status } = useSession()
     if (status === "authenticated") {
-        // if user is authenticated, then input is possible: 
+        // if user is authenticated, then input is possible
 
+        // const { mutate } = useSWR("/api/jokes");
+        // const GratitudeSubmit = async (e) => {
+        //     e.preventDefault();
+        //     const formData = new FormData(e.target);
+        //     const gratitudeData = Object.fromEntries(formData);
+        //     console.log("------ gratitudeData:", gratitudeData);
+        //     const response = await fetch("/api/jokes", {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify(jokeData),
+        //     });
+        //     if (response.ok) {
+        //         mutate();
+        //     }
+        // };
 
-
-
-    } else {
-        // unauthenticated & loading the "displayformbutton" is disabled and a message is shown that you need to sign in to create a gratitude statement 
+        return (
+            <>
+                <form
+                    className="form w-full max-w-sm"
+                    onSubmit={(event) => onGratitudeSubmit(event)}
+                >
+                    <div className="flex items-center border-b border-white py-2 bg-transparent">
+                        <label htmlFor="formTextInput">
+                        </label>
+                        <input
+                            name="formStatementText"
+                            id="formTextInput"
+                            placeholder="What are you grateful for...?"
+                            maxLength={150}
+                            required
+                            className="appearance-none bg-transparent border-none w-full text-blue-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        ></input>
+                        <button type="submit"
+                            className="submit-button flex-shrink-0 bg-transparent hover:bg-gray-80 text-sm text-white py-1 px-2 rounded shadow">
+                            ADD
+                        </button>
+                    </div>
+                </form >
+            </>
+        )
     }
-    // const { mutate } = useSWR("/api/jokes");
-    // const GratitudeSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const formData = new FormData(e.target);
-    //     const gratitudeData = Object.fromEntries(formData);
-    //     console.log("------ gratitudeData:", gratitudeData);
-    //     const response = await fetch("/api/jokes", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(jokeData),
-    //     });
-    //     if (response.ok) {
-    //         mutate();
-    //     }
-    // };
-
-    return (
-        <>
-            <form
-                className="form w-full max-w-sm"
-            // onSubmit={(e) => onGratitudeSubmit(e)}
-            >
-                <div className="flex items-center border-b border-white py-2 bg-transparent">
-                    <label htmlFor="formTextInput">
-                    </label>
-                    <input
-                        name="formStatementText"
-                        id="formTextInput"
-                        placeholder="What are you grateful for...?"
-                        maxLength={150}
-                        required
-                        className="appearance-none bg-transparent border-none w-full text-blue-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    ></input>
-                    <button type="submit"
-                        className="submit-button flex-shrink-0 bg-transparent hover:bg-gray-80 text-sm text-white py-1 px-2 rounded shadow">
-                        ADD
-                    </button>
-                </div>
-            </form >
-        </>
-    )
 }
