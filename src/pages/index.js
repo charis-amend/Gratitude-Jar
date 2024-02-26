f6556bb8de7fc983e520add6fecfafbf95dec80c
Charis Amend <charis.amend@googlemail.com>
2024-02-26 17:31:27 +0100

    added width auto to preloading image for webiste

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,9 @@ export default function Home() {
         <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
           {/* <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" /> */}
           {/* image for the loading page showing this image before the 3D object has loaded  */}
-          <Image src="/assets/preloadedGlasJar.png" alt="placeholder glasjar" width={400} height={600} />
+          <div className="image-container relative h-2/4 place-self-center">
+            <Image src="/assets/preloadedGlasJar.png" alt="placeholder-glasjar" priority fill={true} style={{ objectFit: "contain" }} />
+          </div>
           <div className="login-info-section z-5 fixed top-0.5 right-0.5 z-50 p-4 flex flex-col justify-end">
             <Login />
           </div>
@@ -71,7 +73,10 @@ export default function Home() {
       <>
         <div className="backgroundapp z-0 top-0 left-0 fixed h-screen w-screen flex">  {/* css gradient background: */}
           {/* <GlassJar className="glassjar-spreading-page top-0 left-0 fixed h-screen w-screen z-1" /> */}
-          <Image src="/assets/preloadedGlasJar.png" alt="placeholder glasjar" width={400} height={600} />
+          <div className="image-container relative h-2/4 place-self-center">
+            <Image src="/assets/preloadedGlasJar.png" alt="placeholder glasjar" width={636} height={832} priority />
+          </div>
+
           {/* lower section: */}
           <div className="lower-section fixed top-3/4 left-3.5 right-3.5  bottom-10 z-5 p-4 flex flex-col justify-center items-center">
             <p
