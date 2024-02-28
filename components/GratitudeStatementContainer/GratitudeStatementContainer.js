92bbb487ba21a20f2fb606b48805cff0923202cd
Charis Amend <charis.amend@googlemail.com>
2024-02-28 23:40:02 +0100

    added sass for special viewbox component

--- components/GratitudeStatementContainer/GratitudeStatementContainer.js
+++ components/GratitudeStatementContainer/GratitudeStatementContainer.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import classes from "./GratitudeStatementContainer.module.css"
+import classes from "./GratitudeStatementContainer.module.scss"
 
 export default function GratitudeStatementContainer({ randomGratitudeStatement, onClose }) {
     const [closeContainer, setCloseContainer] = useState(false)
@@ -24,27 +24,8 @@ export default function GratitudeStatementContainer({ randomGratitudeStatement, 
                 <div className="outside-viewbox z-0 top-0 left-0 fixed h-screen w-screen flex"
                     onClick={handleClose}>
                     <div
-                        id="viewbox"
-                        style={{
-                            zIndex: "10",
-                            margin: "3px",
-                            marginTop: "60px",
-                            padding: "3px",
-                            position: "fixed",
-                            right: "50px",
-                            left: "50px",
-                            bottom: "250px",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                            backgroundColor: "rgba(147, 140, 129, 0.9)",
-                            borderRadius: "8px",
-                            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
-                            transition: "opacity 1s", // Changed duration to 2s
-                            opacity: !showViewBox ? 1 : 0,
-                            transitionDelay: !showViewBox ? "0.5s" : "0" // Delay the transition if fading out
-                        }}>
+                        className="viewbox">
+
                         <button
                             type="button"
                             id="closeButton"
